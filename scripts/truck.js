6ce105b6307ab6425d8775e8e49de8ed30f78e67
Chris Underwood <cunderwoodmn@gmail.com>
2017-01-09 21:07:17 -0800

    filled out Truck function and created create order prototype

--- scripts/truck.js
+++ scripts/truck.js
@@ -2,9 +2,15 @@
   'use strict';
   var App = window.App || {};
 
-  function Truck() {
-
+  function Truck(truckId, db) {
+    this.truckId = truckId;
+    this.db = db;
   }
+
+  Truck.prototype.createOrder = function (order) {
+    console.log("Adding order for " + order.emailAddress);
+    this.db.add(order.emailAddress, order);
+  };
 
   App.Truck = Truck;
   window.App = App;

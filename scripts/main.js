a415255fa06989fc8a6103043b1a64989e5a3231
Chris Underwood <cunderwoodmn@gmail.com>
2017-01-14 22:36:07 -0800

    add 4th coffee size category and show the number of the slider caffeine strength on the screen

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,23 @@
   console.log(formHandler);
 
 })(window);
+
+
+var rangeSlider = function() {
+  var slider = $('.range-slider');
+  var range = $('.range-slider__range');
+  var value = $('.range-slider__value');
+
+  slider.each(function() {
+
+    value.each(function() {
+      var value = $(this).prev().attr('value');
+      $(this).html(value);
+    });
+
+    range.on('input', function() {
+      $(this).next(value).html(this.value);
+    });
+  });
+};
+rangeSlider();

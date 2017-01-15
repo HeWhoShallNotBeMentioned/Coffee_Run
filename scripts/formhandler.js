238583e47b9e67676199003eef87e93c0413b48a
Chris Underwood <cunderwoodmn@gmail.com>
2017-01-14 16:20:33 -0800

    add error handlers for FormHandler

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -3,8 +3,14 @@
   var App = window.App || {};
   var $ = window.jQuery;
 
-  function FormHandler() {
-
+  function FormHandler(selector) {
+    if(!selector) {
+      throw new Error('No selector provided');
+    }
+    this.$formElement = $(selector);
+    if (this.formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
   }
 
   App.FormHandler = FormHandler;
